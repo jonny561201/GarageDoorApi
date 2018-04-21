41b700dfa6b4c79e2d775021ea2a5e488b2f9e5f
Jonathon Graf <jgraf@pillartechnology.com>
2018-04-21 18:13:34 -0500

    added the absolute reference to the python interpreter

---
+++ svc/app.py
@@ -0,0 +1,13 @@
+#!/Users/jonathongraf/.pyenv/versions/garage_door_env/bin/python
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
