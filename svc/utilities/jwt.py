854d90959b580224e8d8d118dc46d72e354280b7
Jonathon Graf <jgraf@pillartechnology.com>
2018-05-12 16:10:19 -0500

    added jwt validator to return tru

---
+++ svc/utilities/jwt.py
@@ -0,0 +1,2 @@
+def is_jwt_valid(jwt_token):
+    return True
---
+++ test/unit/utilities/test_jwt.py
@@ -0,0 +1,23 @@
+import os
+
+import jwt
+from svc.utilities.jwt import is_jwt_valid
+
+
+class TestJwt():
+    JWT_BODY = {'fakeBody': 'valueValue'}
+    JWT_SECRET = 'testSecret'
+
+    def setup_method(self, _):
+        os.environ.update({'JWT_SECRET': self.JWT_SECRET})
+
+    def teardown_method(self, _):
+        os.environ.pop('JWT_SECRET')
+
+    def test_is_jwt_valid__should_return_true_if_it_can_be_decrypted(self):
+        jwt_token = jwt.encode(self.JWT_BODY, self.JWT_SECRET, algorithm='HS256')
+
+        acutal = is_jwt_valid(jwt_token)
+
+        assert acutal is True
+
