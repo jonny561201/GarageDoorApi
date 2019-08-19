6e469916a21466159b544a5d7a7770341471aac8
Jon Graf <jongraf1@gmail.com>
2019-08-19 18:32:45 -0500

    updated run all tests to orchestrate behavior better and wait for contianer to be up and healthy

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+WHITE='\033[0m'
+
 function runUnitTests {
-    echo "---------------Running Unit Tests---------------"
+    echo -e "${YELLOW}---------------Running Unit Tests---------------${WHITE}"
     pytest -s $(pwd)/test/unit
 }
 
-function runIntegrationTests {
-    echo "---------------Running Integration Tests---------------"
+function startPostgresDocker {
+    echo -e "${YELLOW}---------------Running Integration Tests---------------${WHITE}"
     docker-compose up -d
+}
+
+function waitForContainerToBeHealthy {
+    until [[ $(docker inspect postgres-test -f='{{.State.Health.Status}}') = "healthy" ]]; do
+        echo "...waiting for healthy..."
+        sleep 1
+    done
+    echo -e "${GREEN}Container is healthy!${WHITE}"
+}
+
+function runIntegrationTests {
+    echo -e "${YELLOW}---------------Running Integration Tests---------------${WHITE}"
     pytest -s $(pwd)/test/integration
 }
 
 function teardownDocker {
-    echo "---------------Cleaning up Container---------------"
+    echo -e "${YELLOW}---------------Cleaning up Container---------------${WHITE}"
     docker-compose down
 }
 
 runUnitTests
+startPostgresDocker
+waitForContainerToBeHealthy
 runIntegrationTests
+teardownDocker
