9edb76752bd35edd08dd4549a64ae5e6f54d931e
Jon Graf <jongraf1@gmail.com>
2019-08-19 17:03:26 -0500

    added extension to database first so we can use uuids for primary keys for users

--- docker/flyway/db/migration/V1.1__database_init.sql
+++ docker/flyway/db/migration/V1.1__database_init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE garage_door_users (
-    ID serial PRIMARY KEY,
+    ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_name VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL
 );
