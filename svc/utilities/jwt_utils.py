f19fa971858ce1f1b3c791382173af70b5dc1908
Jon Graf <jongraf1@gmail.com>
2019-08-20 17:08:38 -0500

    removed temporary token secret used for testing purposes

--- svc/utilities/jwt_utils.py
+++ svc/utilities/jwt_utils.py
@@ -5,8 +5,6 @@ import jwt
 import pytz
 from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError
 
-TEMP_SECRET = 'temporarySecret'
-
 
 def is_jwt_valid(jwt_token):
     return False if jwt_token is None else _parse_jwt_token(jwt_token)
@@ -14,14 +12,14 @@ def is_jwt_valid(jwt_token):
 
 def create_jwt_token():
     expire_time = datetime.now(tz=pytz.timezone('US/Central')) + timedelta(hours=2)
-    jwt_secret = os.environ.get('JWT_SECRET', TEMP_SECRET)
+    jwt_secret = os.environ['JWT_SECRET']
     return jwt.encode({'user_id': 12345, 'exp': expire_time}, jwt_secret, algorithm='HS256')
 
 
 def _parse_jwt_token(jwt_token):
     try:
         stripped_token = jwt_token.replace('Bearer ', '')
-        secret = os.environ.get('JWT_SECRET', TEMP_SECRET)
+        secret = os.environ['JWT_SECRET']
         jwt.decode(stripped_token, secret, algorithms=["HS256"])
         return True
     except (InvalidSignatureError, ExpiredSignatureError, DecodeError, KeyError) as er:
