12de7721e58f88bfede807143385dd38e532c11f
Jon Graf <jongraf1@gmail.com>
2019-08-23 17:54:41 -0500

    added test to serve as documentation

--- test/unit/utilities/test_credentials.py
+++ test/unit/utilities/test_credentials.py
@@ -14,3 +14,16 @@ def test_extract_credentials__should_return_valid_credentials():
 
     assert actual_pass == pword
     assert actual_user == user
+
+
+def test_extract_credentials__should_return_valid_credentials_when_missing_basic():
+    user = "user"
+    pword = "password"
+    creds = "%s:%s" % (user, pword)
+    encoded_token = base64.b64encode(creds.encode())
+    bearer_token = encoded_token.decode('UTF-8')
+
+    actual_user, actual_pass = extract_credentials(bearer_token)
+
+    assert actual_pass == pword
+    assert actual_user == user
