edf58527d07ab6e0d4a0ebf485c8faf00333890c
Jon Graf <jgraf@leantechniques.com>
2019-09-07 16:17:05 -0500

    sump routes will now return the depth data

--- svc/routes/sump_routes.py
+++ svc/routes/sump_routes.py
@@ -2,4 +2,5 @@ from svc.controllers.sump_controller import get_sump_level
 
 
 def get_current_sump_level(user_id):
-    get_sump_level(user_id)
+    depth = get_sump_level(user_id)
+    return depth
--- test/unit/routes/test_sump_routes.py
+++ test/unit/routes/test_sump_routes.py
@@ -9,3 +9,14 @@ def test_get_current_sump_level__should_call_controller(mock_controller):
     get_current_sump_level(user_id)
 
     mock_controller.assert_called_with(user_id)
+
+
+@patch('svc.routes.sump_routes.get_sump_level')
+def test_get_current_sump_level__should_return_valid_response(mock_controller):
+    user_id = 'fakeuserid'
+    expected_depth = {'currentDepth': 1234}
+    mock_controller.return_value = expected_depth
+
+    actual = get_current_sump_level(user_id)
+
+    assert actual == expected_depth
