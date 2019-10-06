19bbf5c464b4aff4056702622d677376cc61cdea
Jon Graf <jongraf1@gmail.com>
2019-10-06 14:34:35 -0500

    added CORS support to flask application server

--- svc/manager.py
+++ svc/manager.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from svc.routes.sump_routes import SUMP_BLUEPRINT
 from svc.routes.app_routes import APP_BLUEPRINT
@@ -9,6 +10,7 @@ from svc.routes.thermostat_routes import THERMOSTAT_BLUEPRINT
 def create_app(app_name):
     if app_name == '__main__':
         app = Flask(app_name)
+        CORS(app)
         app.register_blueprint(APP_BLUEPRINT)
         app.register_blueprint(SUMP_BLUEPRINT)
         app.register_blueprint(THERMOSTAT_BLUEPRINT)
