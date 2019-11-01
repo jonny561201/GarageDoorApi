98ddad027db00a55ee38585455463bcf303aee5a
Jon Graf <jgraf@leantechniques.com>
2019-11-01 16:58:09 -0500

    updated token name to reflect it is basic auth

--- svc/controllers/app_controller.py
+++ svc/controllers/app_controller.py
@@ -3,8 +3,8 @@ from svc.utilities.credentials import extract_credentials
 from svc.utilities.jwt_utils import create_jwt_token
 
 
-def get_login(bearer_token):
-    user_name, pword = extract_credentials(bearer_token)
+def get_login(basic_token):
+    user_name, pword = extract_credentials(basic_token)
     with UserDatabaseManager() as user_database:
         user_id = user_database.validate_credentials(user_name, pword)
         return create_jwt_token(user_id)
