b33c341aff62447d33de10fb713487097b60835b
Jon Graf <jongraf1@gmail.com>
2019-11-01 20:41:02 -0500

    updated flyway scripts to include sump test data and auto generate dates for sump tables

---
+++ docker/flyway/migration/V1.7__sump_tables_auto_generate_date.sql
@@ -0,0 +1,3 @@
+ALTER TABLE daily_sump_level ALTER create_date SET default current_timestamp;
+
+ALTER TABLE average_daily_sump_level ALTER create_day SET default current_date;
--- docker/flyway/migration/V1.8__add_test_data.sql
+++ docker/flyway/migration/V1.8__add_test_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user_information (first_name, last_name, email) VALUES
-('Jon', 'Tester', 'fake1234@gmail.com');
+INSERT INTO user_information (first_name, last_name, email, id) VALUES
+('Jon', 'Tester', 'fake1234@gmail.com', 'e97febc0-fd10-11e9-8f0b-362b9e155667');
 
 INSERT INTO user_login (user_name, password, user_id, role_id) VALUES
 ('Jonny561201', 'password', (SELECT ID FROM user_information WHERE first_name = 'Jon'), (SELECT ID FROM user_roles WHERE role_name = 'garage_door')),
@@ -7,3 +7,6 @@ INSERT INTO user_login (user_name, password, user_id, role_id) VALUES
 ('dingDongFoo', 'obviouslyPassword1', (SELECT ID FROM user_information WHERE first_name = 'Jon'), (SELECT ID FROM user_roles WHERE role_name = 'sump_pump'));
 
 INSERT INTO user_preferences (is_fahrenheit, user_id) VALUES (TRUE, (SELECT ID FROM user_information WHERE last_name = 'Tester'));
+
+INSERT INTO daily_sump_level (distance, user_id) VALUES (31.7, 'e97febc0-fd10-11e9-8f0b-362b9e155667');
+INSERT INTO average_daily_sump_level (distance, user_id) VALUES (33.4, 'e97febc0-fd10-11e9-8f0b-362b9e155667');
