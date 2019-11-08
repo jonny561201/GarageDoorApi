43c53f86d92b4b0c803ee08a5748f2eb3785b7e2
Jon Graf <jgraf@leantechniques.com>
2019-11-08 17:05:14 -0600

    thermostat controller now calls weather api to gather data

--- svc/controllers/thermostat_controller.py
+++ svc/controllers/thermostat_controller.py
@@ -2,6 +2,7 @@ from svc.db.methods.user_credentials import UserDatabaseManager
 from svc.utilities.gpio import read_temperature_file
 from svc.utilities.jwt_utils import is_jwt_valid
 from svc.utilities.temperature import get_user_temperature
+from svc.utilities.api_requests import get_weather_by_city
 
 
 def get_user_temp(user_id, bearer_token):
@@ -10,6 +11,7 @@ def get_user_temp(user_id, bearer_token):
         preference = database.get_preferences_by_user(user_id)
         temp_text = read_temperature_file()
         temperature = get_user_temperature(temp_text, preference.is_fahrenheit)
+        get_weather_by_city(None, None, None)
 
         return {'currentTemp': temperature,
                 'isFahrenheit': preference.is_fahrenheit}
--- test/unit/controllers/test_thermostat_controller.py
+++ test/unit/controllers/test_thermostat_controller.py
@@ -7,6 +7,7 @@ from svc.controllers.thermostat_controller import get_user_temp
 from svc.db.models.user_information_model import UserPreference
 
 
+@patch('svc.controllers.thermostat_controller.get_weather_by_city')
 @patch('svc.controllers.thermostat_controller.UserDatabaseManager')
 @patch('svc.controllers.thermostat_controller.is_jwt_valid')
 @patch('svc.controllers.thermostat_controller.read_temperature_file')
@@ -15,12 +16,12 @@ class TestThermostatController:
     JWT_TOKEN = jwt.encode({}, 'JWT_SECRET', algorithm='HS256').decode('UTF-8')
     USER_ID = uuid.uuid4().hex
 
-    def test_get_user_temp__should_call_read_temperature_file(self, mock_user, mock_file, mock_jwt, mock_db):
+    def test_get_user_temp__should_call_read_temperature_file(self, mock_user, mock_file, mock_jwt, mock_db, mock_weather):
         get_user_temp(self.USER_ID, self.JWT_TOKEN)
 
         mock_file.assert_called()
 
-    def test_get_user_temp__should_call_get_user_temperature(self, mock_user, mock_file, mock_jwt, mock_db):
+    def test_get_user_temp__should_call_get_user_temperature(self, mock_user, mock_file, mock_jwt, mock_db, mock_weather):
         preference = UserPreference()
         preference.is_fahrenheit = True
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.return_value = preference
@@ -31,7 +32,7 @@ class TestThermostatController:
 
         mock_user.assert_called_with(temp_text, True)
 
-    def test_get_user_temp__should_call_is_jwt_valid(self, mock_user, mock_file, mock_jwt, mock_db):
+    def test_get_user_temp__should_call_is_jwt_valid(self, mock_user, mock_file, mock_jwt, mock_db, mock_weather):
         temp_text = 'fake response'
         mock_file.return_value = temp_text
 
@@ -40,7 +41,7 @@ class TestThermostatController:
         mock_jwt.assert_called_with(self.JWT_TOKEN)
 
     def test_get_user_temp__should_return_response_from_get_user_temperature(self, mock_user, mock_file, mock_jwt,
-                                                                             mock_db):
+                                                                             mock_db, mock_weather):
         preference = UserPreference()
         preference.is_fahrenheit = False
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.return_value = preference
@@ -51,13 +52,13 @@ class TestThermostatController:
 
         assert actual == {'currentTemp': expected_temp, 'isFahrenheit': False}
 
-    def test_get_user_temp__should_call_get_preferences_by_user(self, mock_user, mock_file, mock_jwt, mock_db):
+    def test_get_user_temp__should_call_get_preferences_by_user(self, mock_user, mock_file, mock_jwt, mock_db, mock_weather):
         get_user_temp(self.USER_ID, self.JWT_TOKEN)
 
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.assert_called_with(self.USER_ID)
 
     def test_get_user_temp__should_call_get_user_temperature_with_user_id(self, mock_user, mock_file, mock_jwt,
-                                                                          mock_db):
+                                                                          mock_db, mock_weather):
         preference = UserPreference()
         expected_text = 'fake text'
         preference.is_fahrenheit = True
@@ -67,3 +68,8 @@ class TestThermostatController:
         get_user_temp(self.USER_ID, self.JWT_TOKEN)
 
         mock_user.assert_called_with(expected_text, True)
+
+    def test_get_user_temp__should_call_api_requests(self, mock_user, mock_file, mock_jwt, mock_db, mock_weather):
+        get_user_temp(self.USER_ID, self.JWT_TOKEN)
+
+        mock_weather.assert_called_with(None, None, None)
