3391704c5c8a923b888576c344fe64e7f52b55a5
Jon Graf <jgraf@leantechniques.com>
2019-11-10 15:22:34 -0600

    added city to user preferences for test data

--- docker/flyway/migration/V1.20__add_test_data.sql
+++ docker/flyway/migration/V1.20__add_test_data.sql
@@ -6,7 +6,7 @@ INSERT INTO user_login (user_name, password, user_id, role_id) VALUES
 ('l33t', 'password1', (SELECT ID FROM user_information WHERE first_name = 'Jon'), (SELECT ID FROM user_roles WHERE role_name = 'security')),
 ('dingDongFoo', 'obviouslyPassword1', (SELECT ID FROM user_information WHERE first_name = 'Jon'), (SELECT ID FROM user_roles WHERE role_name = 'sump_pump'));
 
-INSERT INTO user_preferences (is_fahrenheit, user_id) VALUES (TRUE, (SELECT ID FROM user_information WHERE last_name = 'Tester'));
+INSERT INTO user_preferences (is_fahrenheit, user_id, city) VALUES (TRUE, (SELECT ID FROM user_information WHERE last_name = 'Tester'), 'Des Moines');
 
 INSERT INTO daily_sump_level (distance, user_id, warning_level) VALUES (31.7, 'e97febc0-fd10-11e9-8f0b-362b9e155667', 3);
 INSERT INTO average_daily_sump_level (distance, user_id) VALUES (33.4, 'e97febc0-fd10-11e9-8f0b-362b9e155667');
