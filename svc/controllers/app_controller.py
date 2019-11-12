69d8ccbc75db66defbf26933b588f25cd707f6d5
Jon Graf <jgraf@leantechniques.com>
2019-11-12 09:49:38 -0600

    created controller to get user preferences using user id

--- svc/controllers/app_controller.py
+++ svc/controllers/app_controller.py
@@ -1,6 +1,6 @@
 from svc.db.methods.user_credentials import UserDatabaseManager
 from svc.utilities.credentials import extract_credentials
-from svc.utilities.jwt_utils import create_jwt_token
+from svc.utilities.jwt_utils import create_jwt_token, is_jwt_valid
 
 
 def get_login(basic_token):
@@ -8,3 +8,7 @@ def get_login(basic_token):
     with UserDatabaseManager() as user_database:
         user_id = user_database.validate_credentials(user_name, pword)
         return create_jwt_token(user_id)
+
+
+def get_user_preferences(bearer_token, user_id):
+    is_jwt_valid(bearer_token)
--- test/unit/controllers/test_app_controller.py
+++ test/unit/controllers/test_app_controller.py
@@ -6,7 +6,7 @@ import jwt
 import pytz
 from mock import patch
 
-from svc.controllers.app_controller import get_login
+from svc.controllers.app_controller import get_login, get_user_preferences
 
 
 @patch('svc.controllers.app_controller.extract_credentials')
@@ -24,28 +24,28 @@ class TestLoginController:
         os.environ.pop('JWT_SECRET')
 
     @patch('svc.controllers.app_controller.create_jwt_token')
-    def test_garage_door_login__should_call_validate_credentials_with_post_body(self, mock_token, mock_credentials, mock_extract):
+    def test_get_login__should_call_validate_credentials_with_post_body(self, mock_token, mock_credentials, mock_extract):
         mock_extract.return_value = (self.USER, self.PWORD)
         get_login(self.JWT_TOKEN)
 
         mock_credentials.return_value.__enter__.return_value.validate_credentials.assert_called_with(self.USER, self.PWORD)
 
     @patch('svc.controllers.app_controller.create_jwt_token')
-    def test_garage_door_login__should_call_extract_credentials(self, mock_token, mock_credentials, mock_extract):
+    def test_get_login__should_call_extract_credentials(self, mock_token, mock_credentials, mock_extract):
         mock_extract.return_value = (self.USER, self.PWORD)
         get_login(self.JWT_TOKEN)
 
         mock_extract.assert_called_with(self.JWT_TOKEN)
 
     @patch('svc.controllers.app_controller.create_jwt_token')
-    def test_garage_door_login__should_call_create_jwt_token(self, mock_token, mock_credentials, mock_extract):
+    def test_get_login__should_call_create_jwt_token(self, mock_token, mock_credentials, mock_extract):
         mock_extract.return_value = (self.USER, self.PWORD)
         get_login(self.JWT_TOKEN)
 
         mock_token.assert_called()
 
     @patch('svc.controllers.app_controller.create_jwt_token')
-    def test_garage_door_login__should_return_response_from_jwt_service(self, mock_token, mock_credentials, mock_extract):
+    def test_get_login__should_return_response_from_jwt_service(self, mock_token, mock_credentials, mock_extract):
         mock_extract.return_value = (self.USER, self.PWORD)
         mock_token.return_value = self.JWT_TOKEN
         actual = get_login(self.JWT_TOKEN)
@@ -53,7 +53,7 @@ class TestLoginController:
         assert actual == self.JWT_TOKEN
 
     @patch('svc.utilities.jwt_utils.datetime')
-    def test_garage_door_login__should_respond_with_jwt_token(self, mock_datetime, mock_credentials, mock_extract):
+    def test_get_login__should_respond_with_jwt_token(self, mock_datetime, mock_credentials, mock_extract):
         mock_extract.return_value = (self.USER, self.PWORD)
         now = datetime.now(tz=pytz.timezone('US/Central'))
         mock_datetime.now.return_value = now
@@ -66,3 +66,11 @@ class TestLoginController:
         actual = get_login("junkToken")
 
         assert jwt.decode(actual, self.JWT_SECRET, algorithms=["HS256"]) == expected_token
+
+    @patch('svc.controllers.app_controller.is_jwt_valid')
+    def test_get_user_preferences__should_validate_bearer_token(self, mock_jwt, mock_db, mock_creds):
+        user_id = 'fakeUserId'
+        bearer_token = 'fake bearer token'
+        get_user_preferences(bearer_token, user_id)
+
+        mock_jwt.assert_called_with(bearer_token)
