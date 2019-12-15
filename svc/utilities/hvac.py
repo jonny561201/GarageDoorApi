24b4ed787a8fec9b30178b9d29f330475d7ffb6f
Jon Graf <jgraf@leantechniques.com>
2019-12-15 13:32:38 -0600

    hvac utility will turn on the hvac when wanting to run ac

--- svc/utilities/hvac.py
+++ svc/utilities/hvac.py
@@ -1,8 +1,11 @@
 # going to assume temperature is already converted to celsius by controller
 from svc.utilities.gpio import read_temperature_file
 from svc.utilities.temperature import get_user_temperature
+from svc.utilities import gpio
 
 
 def run_temperature_program(desired_temp):
     temp_file = read_temperature_file()
     get_user_temperature(temp_file, False)
+
+    gpio.turn_on_hvac()
--- test/unit/utilities/test_hvac.py
+++ test/unit/utilities/test_hvac.py
@@ -3,6 +3,7 @@ from mock import patch, ANY
 from svc.utilities.hvac import run_temperature_program
 
 
+@patch('svc.utilities.hvac.gpio')
 @patch('svc.utilities.hvac.get_user_temperature')
 @patch('svc.utilities.hvac.read_temperature_file')
 class TestHvac:
@@ -10,24 +11,30 @@ class TestHvac:
     AC_TEMP = 35.0
     HEAT_TEMP = 31.0
 
-    def test_run_temperature_program__should_make_call_to_read_temperature_file(self, mock_temp, mock_convert):
+    def test_run_temperature_program__should_make_call_to_read_temperature_file(self, mock_temp, mock_convert, mock_gpio):
         run_temperature_program(self.DESIRED_TEMP)
         mock_temp.assert_called()
 
-    def test_run_temperature_program__should_call_get_user_temperature(self, mock_temp, mock_convert):
+    def test_run_temperature_program__should_call_get_user_temperature(self, mock_temp, mock_convert, mock_gpio):
         mock_temp.return_value = self.AC_TEMP
 
         run_temperature_program(self.DESIRED_TEMP)
         mock_convert.assert_called()
 
-    def test_run_temperature_program__should_make_call_to_get_user_temperature_with_result_of_temp_file(self, mock_temp, mock_convert):
+    def test_run_temperature_program__should_make_call_to_get_user_temperature_with_result_of_temp_file(self, mock_temp, mock_convert, mock_gpio):
         mock_temp.return_value = self.AC_TEMP
 
         run_temperature_program(self.DESIRED_TEMP)
         mock_convert.assert_called_with(self.AC_TEMP, ANY)
 
-    def test_run_temperature_program__should_make_call_to_get_user_temperature_with_celsius(self, mock_temp, mock_convert):
+    def test_run_temperature_program__should_make_call_to_get_user_temperature_with_celsius(self, mock_temp, mock_convert, mock_gpio):
         mock_temp.return_value = self.AC_TEMP
 
         run_temperature_program(self.DESIRED_TEMP)
         mock_convert.assert_called_with(ANY, False)
+
+    def test_run_temperature_program__should_turn_on_ac_when_temp_above_desired(self, mock_temp, mock_convert, mock_gpio):
+        mock_temp.return_value = self.AC_TEMP
+
+        run_temperature_program(self.DESIRED_TEMP)
+        mock_gpio.turn_on_hvac.assert_called()
