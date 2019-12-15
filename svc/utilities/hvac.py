0db541559fec9af422618388df7d8dfb6266c41c
Jon Graf <jgraf@leantechniques.com>
2019-12-15 13:20:33 -0600

    hvac utility is now making call to get user temperature

--- svc/utilities/hvac.py
+++ svc/utilities/hvac.py
@@ -1,6 +1,8 @@
 # going to assume temperature is already converted to celsius by controller
 from svc.utilities.gpio import read_temperature_file
+from svc.utilities.temperature import get_user_temperature
 
 
 def run_temperature_program(desired_temp):
     read_temperature_file()
+    get_user_temperature()
--- test/unit/utilities/test_hvac.py
+++ test/unit/utilities/test_hvac.py
@@ -3,10 +3,19 @@ from mock import patch
 from svc.utilities.hvac import run_temperature_program
 
 
+@patch('svc.utilities.hvac.get_user_temperature')
 @patch('svc.utilities.hvac.read_temperature_file')
 class TestHvac:
-    DESIRED_TEMP = 32.9
+    DESIRED_TEMP = 33.0
+    AC_TEMP = 35.0
+    HEAT_TEMP = 31.0
 
-    def test_run_temperature_program__should_make_call_to_read_temperature_file(self, mock_temp):
+    def test_run_temperature_program__should_make_call_to_read_temperature_file(self, mock_temp, mock_convert):
         run_temperature_program(self.DESIRED_TEMP)
         mock_temp.assert_called()
+
+    def test_run_temperature_program__should_call_get_user_temperature(self, mock_temp, mock_convert):
+        mock_temp.return_value = self.AC_TEMP
+
+        run_temperature_program(self.DESIRED_TEMP)
+        mock_convert.assert_called()
