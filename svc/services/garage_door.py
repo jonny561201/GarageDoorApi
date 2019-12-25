3d31bbf7880f28b9311b27648deb87584806707e
Jon Graf <jgraf@leantechniques.com>
2019-12-24 21:39:32 -0600

    created garage door service to store garage status and datetime

---
+++ svc/services/garage_door.py
@@ -0,0 +1,16 @@
+# if garage door status open and open time null set open time and status
+# if garage door status closed and closed time null set closed time and status
+from datetime import datetime
+
+from svc.constants.garage_state import GarageState
+from svc.constants.home_automation import Automation
+from svc.utilities.gpio import is_garage_open
+
+
+def monitor_status():
+    status = is_garage_open()
+    state = GarageState.get_instance()
+
+    if status:
+        state.STATUS = Automation.GARAGE.OPEN
+        state.OPEN_TIME = datetime.now()
---
+++ test/unit/services/test_garage_door.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from mock import patch
+
+from svc.constants.garage_state import GarageState
+from svc.constants.home_automation import Automation
+from svc.services.garage_door import monitor_status
+
+
+@patch('svc.services.garage_door.datetime')
+@patch('svc.services.garage_door.is_garage_open')
+class TestGarageService:
+
+    DATE = datetime.now()
+    STATE = GarageState.get_instance()
+
+    def test_monitor_status__should_set_status_to_open_when_garage_open(self, mock_status, mock_date):
+        mock_status.return_value = True
+
+        monitor_status()
+
+        assert self.STATE.STATUS == Automation.GARAGE.OPEN
+
+    def test_monitor_status__should_set_open_time_when_garage_open(self, mock_status, mock_date):
+        mock_status.return_value = True
+        mock_date.now.return_value = self.DATE
+
+        monitor_status()
+
+        assert self.STATE.OPEN_TIME == self.DATE
