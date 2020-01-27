0ef10ce75d60d1d56077817332493889d369de31
Jon Graf <jgraf@leantechniques.com>
2020-01-26 21:34:02 -0600

    added new state object to house the settings values

---
+++ svc/constants/settings_state.py
@@ -0,0 +1,26 @@
+import json
+
+
+class Settings:
+    __instance = None
+    settings = None
+
+    def __init__(self):
+        if Settings.__instance is not None:
+            raise Exception
+        else:
+            Settings.__instance = self
+
+    @staticmethod
+    def get_instance():
+        if Settings.__instance is None:
+            Settings.__instance = Settings()
+        return Settings.__instance
+
+    def get_settings(self):
+        if self.settings is None:
+            with open("./settings.json", "r") as reader:
+                self.settings = json.loads(reader.read())
+            return self.settings
+        else:
+            return self.settings
