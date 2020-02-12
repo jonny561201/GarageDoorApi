82f275309e1a640d39eaa3ffc63efe8ab943fd6b
Jon Graf <jongraf1@gmail.com>
2020-02-11 22:29:15 -0600

    get assigned lights will now capture the api key and use it to make api query

--- svc/services/light_request.py
+++ svc/services/light_request.py
@@ -1,9 +1,12 @@
 import os
 
 from svc.utilities.api_requests_utils import get_light_api_key
+from svc.utilities.api_requests_utils import get_light_groups
 
 
 def get_assigned_lights():
     username = os.environ['LIGHT_API_USERNAME']
     password = os.environ['LIGHT_API_PASSWORD']
-    get_light_api_key(username, password)
+    api_key = get_light_api_key(username, password)
+
+    get_light_groups(api_key)
--- test/unit/services/test_light_request.py
+++ test/unit/services/test_light_request.py
@@ -1,14 +1,16 @@
 import os
 
-from mock import patch
+from mock import patch, ANY
 
 from svc.services.light_request import get_assigned_lights
 
 
+@patch('svc.services.light_request.get_light_groups')
 @patch('svc.services.light_request.get_light_api_key')
 class TestLightRequest:
     LIGHT_USERNAME = "fakeUsername"
     LIGHT_PASSWORD = "fakePassword"
+    API_KEY = "fakeApiKey"
 
     def setup_method(self):
         os.environ.update({'LIGHT_API_USERNAME': self.LIGHT_USERNAME})
@@ -18,7 +20,13 @@ class TestLightRequest:
         os.environ.pop('LIGHT_API_USERNAME')
         os.environ.pop('LIGHT_API_PASSWORD')
 
-    def test_get_assigned_lights__should_call_to_get_api_key(self, mock_get_key):
+    def test_get_assigned_lights__should_call_to_get_api_key(self, mock_key, mock_get):
         get_assigned_lights()
 
-        mock_get_key.assert_called_with(self.LIGHT_USERNAME, self.LIGHT_PASSWORD)
+        mock_key.assert_called_with(self.LIGHT_USERNAME, self.LIGHT_PASSWORD)
+
+    def test_get_assigned_lights__should_pass_api_key_to_get_light_groups(self, mock_key, mock_get):
+        mock_key.return_value = self.API_KEY
+        get_assigned_lights()
+
+        mock_get.assert_called_with(self.API_KEY)
