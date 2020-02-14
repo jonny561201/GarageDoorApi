0166cdfcf53e7a4e19ff98bc858ef632e65da831
Jon Graf <jongraf1@gmail.com>
2020-02-14 17:31:47 -0600

    light controller will return the default content type headers

--- svc/routes/light_routes.py
+++ svc/routes/light_routes.py
@@ -3,8 +3,10 @@ from flask import request, Response
 from svc.controllers.light_controller import get_assigned_lights
 
 
+DEFAULT_HEADERS = {'Content-Type': 'text/json'}
+
 def get_all_assigned_lights():
     bearer_token = request.headers.get('Authorization')
     get_assigned_lights(bearer_token)
 
-    return Response(status=200)
+    return Response(status=200, headers=DEFAULT_HEADERS)
--- test/unit/routes/test_light_routes.py
+++ test/unit/routes/test_light_routes.py
@@ -15,8 +15,13 @@ class TestLightRoutes:
 
         mock_controller.assert_called_with(bearer_token)
 
-    def test_get_all_assigned_lights__should_return_status_code_200(self, mock_controller, mock_request):
+    def test_get_all_assigned_lights__should_return_success_status_code(self, mock_controller, mock_request):
         actual = get_all_assigned_lights()
 
         assert actual.status_code == 200
+
+    def test_get_all_assigned_lights__should_return_success_headers(self, mock_controller, mock_request):
+        actual = get_all_assigned_lights()
+
+        assert actual.content_type == 'text/json'
 
