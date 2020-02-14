dc186041b5c27b2412cebb266c3300c82af9d803
Jon Graf <jongraf1@gmail.com>
2020-02-14 17:38:03 -0600

    light route will return response from controller

--- svc/routes/light_routes.py
+++ svc/routes/light_routes.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import request, Response
 
 from svc.controllers.light_controller import get_assigned_lights
@@ -5,8 +7,9 @@ from svc.controllers.light_controller import get_assigned_lights
 
 DEFAULT_HEADERS = {'Content-Type': 'text/json'}
 
+
 def get_all_assigned_lights():
     bearer_token = request.headers.get('Authorization')
-    get_assigned_lights(bearer_token)
+    response = get_assigned_lights(bearer_token)
 
-    return Response(status=200, headers=DEFAULT_HEADERS)
+    return Response(json.dumps(response), status=200, headers=DEFAULT_HEADERS)
--- test/unit/routes/test_light_routes.py
+++ test/unit/routes/test_light_routes.py
@@ -1,3 +1,5 @@
+import json
+
 from mock import patch
 
 from svc.routes.light_routes import get_all_assigned_lights
@@ -10,18 +12,28 @@ class TestLightRoutes:
     def test_get_all_assigned_lights__should_call_get_assigned_lights(self, mock_controller, mock_request):
         bearer_token = 'not real'
         mock_request.headers = {'Authorization': bearer_token}
+        mock_controller.return_value = {}
 
         get_all_assigned_lights()
 
         mock_controller.assert_called_with(bearer_token)
 
     def test_get_all_assigned_lights__should_return_success_status_code(self, mock_controller, mock_request):
+        mock_controller.return_value = {}
         actual = get_all_assigned_lights()
 
         assert actual.status_code == 200
 
     def test_get_all_assigned_lights__should_return_success_headers(self, mock_controller, mock_request):
+        mock_controller.return_value = {}
         actual = get_all_assigned_lights()
 
         assert actual.content_type == 'text/json'
+
+    def test_get_all_assigned_lights__should_response_from_controller(self, mock_controller, mock_request):
+        result = {'response': 'not important'}
+        mock_controller.return_value = result
+        actual = get_all_assigned_lights()
+
+        assert json.loads(actual.data) == result
 
