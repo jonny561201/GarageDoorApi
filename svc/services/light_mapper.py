e58dc31e554709d4645c9597c5d8f344b6930aac
Jon Graf <jongraf1@gmail.com>
2020-02-14 16:54:19 -0600

    updated light mapper to provide the group id on/off state

--- svc/services/light_mapper.py
+++ svc/services/light_mapper.py
@@ -1,6 +1,7 @@
 def map_light_groups(api_response, group_state):
-    return [__map_group(k, v) for k, v in api_response.items()]
+    return [__map_group(k, v, group_state) for k, v in api_response.items()]
 
 
-def __map_group(group_id, group):
-    return {'groupId': group_id, 'groupName': group['name']}
+def __map_group(group_id, group, group_state):
+    id_state = group_state[group_id]['action']['on']
+    return {'groupId': group_id, 'groupName': group['name'], 'on': id_state}
--- test/unit/services/test_light_mapper.py
+++ test/unit/services/test_light_mapper.py
@@ -3,7 +3,7 @@ from svc.services.light_mapper import map_light_groups
 
 def test_map_light_groups__should_map_group_ids_into_list():
     response = {'1': {'name': 'Test'}, '2': {'name': 'other'}}
-    group = {}
+    group = {'1': {'action': {'on': False}}, '2': {'action': {'on': False}}}
 
     actual = map_light_groups(response, group)
 
@@ -13,12 +13,9 @@ def test_map_light_groups__should_map_group_ids_into_list():
 
 
 def test_map_light_groups__should_map_group_name():
-    group = {}
+    group = {'1': {'action': {'on': False}}}
     response = {
         "1": {
-            "devicemembership": [],
-            "etag": "ab5272cfe11339202929259af22252ae",
-            "hidden": False,
             "name": "Living Room"
         }
     }
@@ -26,3 +23,22 @@ def test_map_light_groups__should_map_group_name():
     actual = map_light_groups(response, group)
 
     assert actual[0]['groupName'] == 'Living Room'
+
+
+def test_map_light_groups__should_map_the_group_state():
+    response = {
+        "1": {
+            "name": "Living Room"
+        },
+        "3": {
+            "name": "Dinning Room"
+        }
+    }
+    group_state = {'1': {'action': {'on': False}}, '3': {'action': {'on': True}}}
+    actual = map_light_groups(response, group_state)
+
+    assert actual[0]['groupName'] == 'Living Room'
+    assert actual[0]['on'] is False
+    assert actual[1]['groupName'] == 'Dinning Room'
+    assert actual[1]['on'] is True
+
