004a92fd6eaf384f51a7cc7b0e573abb453d3c18
Jon Graf <jgraf@leantechniques.com>
2020-02-17 21:08:14 -0600

    added light rest route that will call the light controller

--- svc/routes/light_routes.py
+++ svc/routes/light_routes.py
@@ -2,7 +2,7 @@ import json
 
 from flask import request, Response, Blueprint
 
-from svc.controllers.light_controller import get_assigned_lights
+from svc.controllers import light_controller
 
 LIGHT_BLUEPRINT = Blueprint('light_blueprint', __name__)
 DEFAULT_HEADERS = {'Content-Type': 'text/json'}
@@ -11,6 +11,11 @@ DEFAULT_HEADERS = {'Content-Type': 'text/json'}
 @LIGHT_BLUEPRINT.route('/lights/groups', methods=['GET'])
 def get_all_assigned_lights():
     bearer_token = request.headers.get('Authorization')
-    response = get_assigned_lights(bearer_token)
+    response = light_controller.get_assigned_lights(bearer_token)
 
     return Response(json.dumps(response), status=200, headers=DEFAULT_HEADERS)
+
+
+def set_assigned_light_group():
+    bearer_token = request.headers.get('Authorization')
+    light_controller.set_assigned_lights(bearer_token, json.loads(request.data))
--- test/unit/routes/test_light_routes.py
+++ test/unit/routes/test_light_routes.py
@@ -2,38 +2,47 @@ import json
 
 from mock import patch
 
-from svc.routes.light_routes import get_all_assigned_lights
+from svc.routes.light_routes import get_all_assigned_lights, set_assigned_light_group
 
 
 @patch('svc.routes.light_routes.request')
-@patch('svc.routes.light_routes.get_assigned_lights')
+@patch('svc.routes.light_routes.light_controller')
 class TestLightRoutes:
 
     def test_get_all_assigned_lights__should_call_get_assigned_lights(self, mock_controller, mock_request):
         bearer_token = 'not real'
         mock_request.headers = {'Authorization': bearer_token}
-        mock_controller.return_value = {}
+        mock_controller.get_assigned_lights.return_value = {}
 
         get_all_assigned_lights()
 
-        mock_controller.assert_called_with(bearer_token)
+        mock_controller.get_assigned_lights.assert_called_with(bearer_token)
 
     def test_get_all_assigned_lights__should_return_success_status_code(self, mock_controller, mock_request):
-        mock_controller.return_value = {}
+        mock_controller.get_assigned_lights.return_value = {}
         actual = get_all_assigned_lights()
 
         assert actual.status_code == 200
 
     def test_get_all_assigned_lights__should_return_success_headers(self, mock_controller, mock_request):
-        mock_controller.return_value = {}
+        mock_controller.get_assigned_lights.return_value = {}
         actual = get_all_assigned_lights()
 
         assert actual.content_type == 'text/json'
 
     def test_get_all_assigned_lights__should_response_from_controller(self, mock_controller, mock_request):
         result = {'response': 'not important'}
-        mock_controller.return_value = result
+        mock_controller.get_assigned_lights.return_value = result
         actual = get_all_assigned_lights()
 
         assert json.loads(actual.data) == result
 
+    def test_set_assigned_light_group__should_call_light_controller(self, mock_controller, mock_request):
+        bearer_token = 'fakeBearer'
+        request_data = '{"on": "False", "groupId": 1}'
+        mock_request.headers = {'Authorization': bearer_token}
+        mock_request.data = request_data.encode()
+
+        set_assigned_light_group()
+
+        mock_controller.set_assigned_lights.assert_called_with(bearer_token, json.loads(request_data))
