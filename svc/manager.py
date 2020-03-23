4b5fc1504ed427305d86a6907656a4870d1c8d29
Jon Graf <jgraf@leantechniques.com>
2020-03-22 22:17:04 -0500

    removed flask cors from the app as backend api will not need to run cors

--- svc/manager.py
+++ svc/manager.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_cors import CORS
 
 from svc.routes.garage_door_routes import GARAGE_BLUEPRINT
 
@@ -7,7 +6,6 @@ from svc.routes.garage_door_routes import GARAGE_BLUEPRINT
 def create_app(app_name):
     if app_name == '__main__':
         app = Flask(app_name)
-        CORS(app)
         app.register_blueprint(GARAGE_BLUEPRINT)
 
         return app
