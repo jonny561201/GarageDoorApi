51fab378a2aa51560f42de2cc8ae303b593f69aa
Jon Graf <jongraf1@gmail.com>
2020-07-31 15:16:43 -0500

    updated bash scripts to work across os platforms

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -8,27 +8,64 @@ WHITE='\033[0m'
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function runUnitTests {
-    echo -e "${YELLOW}---------------Running Unit Tests---------------${WHITE}"
-    python -m pytest -s ${CURRENT_DIR}/test/unit
-    UNIT_TEST=$?
-    if [[ ${UNIT_TEST} -ne 0 ]]; then
-        echo -e "${RED}ERROR: Unit Tests Failed!!!${WHITE}"
-        exit 1
+    echo -e "${YELLOW}----------Running Unit Tests----------${WHITE}"
+    echo ${CURRENT_DIR}
+    source ./venv/Scripts/activate
+    if [[ "$OSTYPE" == "msys" ]]; then
+      python -m pytest -s ${CURRENT_DIR}/test/unit
+      UNIT_TEST=$?
+      if [[ ${UNIT_TEST} -ne 0 ]]; then
+          echo -e "${RED}ERROR: Unit Tests Failed!!!${WHITE}"
+          exit 1
+      fi
+    else
+      python3 -m pytest -s ${CURRENT_DIR}/test/unit
+      UNIT_TEST=$?
+      if [[ ${UNIT_TEST} -ne 0 ]]; then
+          echo -e "${RED}ERROR: Unit Tests Failed!!!${WHITE}"
+          exit 1
+      fi
     fi
-    echo -e "${GREEN}---------------Unit Tests Passed---------------${WHITE}"
+    echo -e "${GREEN}----------Unit Tests Passed----------${WHITE}"
 }
 
 function runIntegrationTests {
     echo -e "${YELLOW}---------------Running Integration Tests---------------${WHITE}"
-    python -m pytest -s ${CURRENT_DIR}/test/integration
-    INTEGRATION_EXIT=$?
-    if [[ ${INTEGRATION_EXIT} -ne 0 ]]; then
-        echo -e "${RED}ERROR: Integration Tests Failed!!!${WHITE}"
-        exit 1
+    if [[ "$OSTYPE" == "msys" ]]; then
+      python -m pytest -s ${CURRENT_DIR}/test/integration
+      INTEGRATION_EXIT=$?
+      if [[ ${INTEGRATION_EXIT} -ne 0 ]]; then
+          echo -e "${RED}ERROR: Integration Tests Failed!!!${WHITE}"
+          exit 1
+      fi
+      echo -e "${GREEN}---------------Integration Tests Passed---------------${WHITE}"
+    else
+      python3 -m pytest -s ${CURRENT_DIR}/test/integration
+      INTEGRATION_EXIT=$?
+      if [[ ${INTEGRATION_EXIT} -ne 0 ]]; then
+          echo -e "${RED}ERROR: Integration Tests Failed!!!${WHITE}"
+          exit 1
+      fi
+      echo -e "${GREEN}---------------Integration Tests Passed---------------${WHITE}"
     fi
-    echo -e "${GREEN}---------------Integration Tests Passed---------------${WHITE}"
+}
+
+function activateVirtualEnv {
+  echo -e "${YELLOW}---------------Activating Virtual Environment---------------${WHITE}"
+  if [[ "$OSTYPE" == "msys" ]]; then
+    source ${CURRENT_DIR}/venv/Scripts/activate
+  else
+    source ${CURRENT_DIR}/venv/bin/activate
+  fi
+}
+
+function deactivateVirtualEnv {
+  echo -e "${YELLOW}---------------Deactivating Virtual Environment---------------${WHITE}"
+  deactivate
 }
 
 
+activateVirtualEnv
 runUnitTests
 runIntegrationTests
+deactivateVirtualEnv
