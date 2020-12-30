21dfc14301fdee8f407f81382b6cf17340c04960
Jon Graf <jongraf1@gmail.com>
2020-12-29 18:17:43 -0600

    rolling back...sigh

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from svc.manager import create_app
+from svc.manager import app
 
 if __name__ == '__main__':
-    app = create_app()
     app.run()
--- svc/manager.py
+++ svc/manager.py
@@ -3,7 +3,6 @@ from flask import Flask
 from svc.routes.garage_door_routes import GARAGE_BLUEPRINT
 
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(GARAGE_BLUEPRINT)
-    return app
+app = Flask(__name__)
+app.register_blueprint(GARAGE_BLUEPRINT)
+
