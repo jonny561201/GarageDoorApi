76af922862491ac223ee5172b62f0782410e167a
Jon Graf <jongraf1@gmail.com>
2021-01-12 00:15:22 -0600

    updated the file utility to read the json file for duration or else return current time

--- svc/utilities/file_utils.py
+++ svc/utilities/file_utils.py
@@ -1,2 +1,16 @@
+import json
+from datetime import datetime
+
+import pytz
+
+from svc.constants.settings_state import Settings
+
+
 def get_door_duration(garage_id):
-    pass
+    file_name = Settings.get_instance().file_name
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            content = json.load(file)
+            return content[garage_id]
+    except FileNotFoundError:
+        return f'{datetime.now(pytz.utc):%Y-%m-%d %H:%M:%S%z}'
