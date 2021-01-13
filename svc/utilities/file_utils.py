48842fade91d6563a3e54e3559d9f257ea37ab9c
Jon Graf <jongraf1@gmail.com>
2021-01-12 18:34:31 -0600

    updated garage door file util to raise a failed dependency error when the garage duration file doesnt exist

--- svc/utilities/file_utils.py
+++ svc/utilities/file_utils.py
@@ -1,7 +1,6 @@
 import json
-from datetime import datetime
 
-import pytz
+from werkzeug.exceptions import FailedDependency
 
 from svc.constants.settings_state import Settings
 
@@ -13,4 +12,4 @@ def get_door_duration(garage_id):
             content = json.load(file)
             return content[garage_id]
     except (FileNotFoundError, TypeError):
-        return f'{datetime.now(pytz.utc):%Y-%m-%d %H:%M:%S%z}'
+        raise FailedDependency()
