7cc0e0926dd45f7572789efea6f45746841a1a61
Jon Graf <jongraf1@gmail.com>
2021-03-01 18:27:16 -0600

    file utility will no longer throw an exception when the file is missing but will now create the file

--- svc/utilities/file_utils.py
+++ svc/utilities/file_utils.py
@@ -1,6 +1,7 @@
 import json
+from datetime import datetime
 
-from werkzeug.exceptions import FailedDependency
+import pytz
 
 from svc.constants.settings_state import Settings
 
@@ -12,4 +13,8 @@ def get_door_duration(garage_id):
             content = json.load(file)
             return content[garage_id]
     except (FileNotFoundError, TypeError):
-        raise FailedDependency()
+        now = datetime.now(pytz.utc)
+        content = {'1': f'{now :%Y-%m-%d %H:%M:%S%z}', '2': f'{now :%Y-%m-%d %H:%M:%S%z}'}
+        with open(file_name, "w+") as file:
+            json.dump(content, file)
+        return content[garage_id]
