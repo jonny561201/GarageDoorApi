aac1f2eaa7dcef2a3b2de13639c045d15c812586
Jon Graf <jgraf@vermeer.com>
2026-01-05 20:27:19 -0600

    updated the flask call to use the mimetype and constants

--- svc/routes/garage_door_routes.py
+++ svc/routes/garage_door_routes.py
@@ -2,28 +2,28 @@ from flask import Response, Blueprint
 from flask import json
 from flask import request
 
+from constants.home_automation import Mime
 from svc.controllers import garage_door_controller
 
 GARAGE_BLUEPRINT = Blueprint('garage_blueprint', __name__, url_prefix='/garageDoor')
-DEFAULT_HEADERS = {'Content-Type': 'application/json'}
 
 
 @GARAGE_BLUEPRINT.route('/<garage_id>/status', methods=['GET'])
 def get_garage_door_status(garage_id):
     bearer_token = request.headers.get('Authorization')
     status = garage_door_controller.get_status(bearer_token, garage_id)
-    return Response(json.dumps(status), status=200, headers=DEFAULT_HEADERS)
+    return Response(json.dumps(status), status=200, mimetype=Mime.JSON)
 
 
 @GARAGE_BLUEPRINT.route('/<garage_id>/state', methods=['POST'])
 def update_garage_door_state(garage_id):
     bearer_token = request.headers.get('Authorization')
     updated_state = garage_door_controller.update_state(bearer_token, garage_id, request.data)
-    return Response(json.dumps(updated_state), status=200, headers=DEFAULT_HEADERS)
+    return Response(json.dumps(updated_state), status=200, mimetype=Mime.JSON)
 
 
 @GARAGE_BLUEPRINT.route('/<garage_id>/toggle', methods=['GET'])
 def toggle_garage_door(garage_id):
     bearer_token = request.headers.get('Authorization')
     garage_door_controller.toggle_door(bearer_token, garage_id)
-    return Response(status=200, headers=DEFAULT_HEADERS)
+    return Response(status=200, mimetype=Mime.JSON)
