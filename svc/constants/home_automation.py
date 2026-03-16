d4059e1be8888a49ffffc69e4d3b1e255f11400d
Jon Graf <jgraf@vermeer.com>
2026-03-16 10:40:47 -0500

    moved the pin configurations to the constants object...should have done sooner

--- svc/constants/home_automation.py
+++ svc/constants/home_automation.py
@@ -11,6 +11,10 @@ class Garage:
     OPEN = True
     CLOSED = False
     QUEUE = 'home-automation-garage'
+    FIRST_STATUS_PIN = 11
+    FIRST_UPDATE_PIN = 31
+    SECOND_STATUS_PIN = 12
+    SECOND_UPDATE_PIN = 33
 
 
 class Timing:
