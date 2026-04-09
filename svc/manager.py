c62e23a6a8ae36bf72d6ca34352bf1eb1645fb50
Jon Graf <jgraf@vermeer.com>
2026-04-09 09:55:09 -0500

    setup the registration process to start up with app

--- svc/manager.py
+++ svc/manager.py
@@ -4,12 +4,16 @@ from svc.config.settings_state import Settings
 from svc.controllers.garage_door_controller import update_door_worker
 from svc.endpoints.app_routes import APP_BLUEPRINT
 from svc.endpoints.garage_door_routes import GARAGE_BLUEPRINT
+from svc.utilities.mdns_registration import MdnsRegistration
 from svc.utilities.rabbitmq_client import RabbitMQClient
 
 
 app = Flask(__name__)
 app.register_blueprint(GARAGE_BLUEPRINT)
 app.register_blueprint(APP_BLUEPRINT)
+
+mdns = MdnsRegistration(port=5001)
+mdns.register()
 
 client = RabbitMQClient(Settings.get_instance())
 client.start_consumer(update_door_worker)
